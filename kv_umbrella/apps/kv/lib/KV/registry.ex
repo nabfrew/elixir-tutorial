8dc634a1b5a50714367657fb6b40a891d7f35002
nabfrew <nabfrew@hotmail.com>
2024-04-16 15:12:40 +0200

    fix bug

--- kv_umbrella/apps/kv/lib/KV/registry.ex
+++ kv_umbrella/apps/kv/lib/KV/registry.ex
@@ -44,13 +44,12 @@ defmodule KV.Registry do
     case lookup(names, name) do
       {:ok, pid} ->
         {:reply, pid, {names, refs}}
-
       :error ->
         {:ok, pid} = DynamicSupervisor.start_child(KV.BucketSupervisor, KV.Bucket)
         ref = Process.monitor(pid)
         refs = Map.put(refs, ref, name)
         :ets.insert(names, {name, pid})
-        {:noreply, {names, refs}}
+        {:reply, pid,{names, refs}}
     end
   end
 

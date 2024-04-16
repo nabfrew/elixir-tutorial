8378c71de67cd5a7631141ac24b7bfbe1c7cfc1f
nabfrew <nabfrew@hotmail.com>
2024-04-16 22:14:10 +0200

    s

--- kv_umbrella/apps/kv_server/lib/kv_server/application.ex
+++ kv_umbrella/apps/kv_server/lib/kv_server/application.ex
@@ -8,12 +8,9 @@ defmodule KVServer.Application do
   @impl true
   def start(_type, _args) do
     children = [
-      # Starts a worker by calling: KVServer.Worker.start_link(arg)
-      # {KVServer.Worker, arg}
+      {Task, fn -> KVServer.accept(4040) end}
     ]
 
-    # See https://hexdocs.pm/elixir/Supervisor.html
-    # for other strategies and supported options
     opts = [strategy: :one_for_one, name: KVServer.Supervisor]
     Supervisor.start_link(children, opts)
   end
